b7f0f3587bd8c542f56bbe50e4b039476c95006e
Jack <xhzengAIB@gmail.com>
2015-07-06 14:31:35 +0800

    添加基本的构造方法

--- XHAmazingLoading/Helpers/XHLayerHelper.m
+++ XHAmazingLoading/Helpers/XHLayerHelper.m
@@ -10,4 +10,36 @@
 
 @implementation XHLayerHelper
 
++ (void)addTextLayerWithText:(NSString *)text atLayer:(CALayer *)layer {
+    CGRect textRect = CGRectMake(47, 82, 100, 40);
+    CATextLayer *labelLayer = [[CATextLayer alloc] init];
+    labelLayer.contentsScale = [[UIScreen mainScreen] scale];
+    labelLayer.font = (__bridge CFStringRef)@"Arial-ItalicMT";
+    [labelLayer setFontSize:30];
+    [labelLayer setFrame:textRect];
+    [labelLayer setString:text];
+    [labelLayer setAlignmentMode:kCAAlignmentCenter];
+    [labelLayer setForegroundColor:[[UIColor blackColor] CGColor]];
+    [layer addSublayer:labelLayer];
+}
+
++ (CAReplicatorLayer *)addReplicatorLayerWithFrame:(CGRect)frame atLayer:(CALayer *)layer {
+    
+    CAReplicatorLayer *replicatorLayer = [CAReplicatorLayer layer];
+    
+    replicatorLayer.frame = frame;
+    
+    [layer addSublayer:replicatorLayer];
+    return replicatorLayer;
+}
+
++ (CGRect)initializerFrameWithSize:(CGSize)size {
+    CGFloat mainHeight = CGRectGetHeight([[UIScreen mainScreen] bounds]);
+    CGFloat mainWdiht = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    CGRect frame = CGRectMake((mainWdiht - size.width) / 2.0,
+                              (mainHeight - size.height) / 2.0,
+                              size.width, size.height);
+    return frame;
+}
+
 @end
