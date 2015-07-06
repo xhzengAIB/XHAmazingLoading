85d2d5c6028c27e08df6fb2b4f18b796f742e83e
Jack <xhzengAIB@gmail.com>
2015-07-06 14:25:31 +0800

    设计构造方法

--- XHAmazingLoading/View/XHAmazingLoadingView.m
+++ XHAmazingLoading/View/XHAmazingLoadingView.m
@@ -7,7 +7,26 @@
 //
 
 #import "XHAmazingLoadingView.h"
+#import "XHAmazingLoadingMusicsAnimation.h"
+#import "XHAmazingLoadingStarAnimation.h"
+
+#define kXHAmazingLoadingDefaultSize 60.0f
+#define kXHAmazingLoadingDefaultTintColor [UIColor colorWithRed:287/255.0f green:105/255.0f blue:121/255.0f alpha:1.0f]
 
 @implementation XHAmazingLoadingView
+
+#pragma mark - Propertys
+
+#pragma mark Getters
+
++ (id <XHAmazingLoadingAnimationProtocol>)amazingLoadingAnimationForAnimationType:(XHAmazingLoadingAnimationType)type {
+    switch (type) {
+        case XHAmazingLoadingAnimationTypeStar:
+            return [[XHAmazingLoadingStarAnimation alloc] init];
+        case XHAmazingLoadingAnimationTypeMusic:
+            return [[XHAmazingLoadingMusicsAnimation alloc] init];
+    }
+    return nil;
+}
 
 @end
