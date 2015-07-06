e197174cf1b59777863496cb01b35b1b3207d1ac
Jack <xhzengAIB@gmail.com>
2015-07-06 14:20:57 +0800

    新建协议头文件

---
+++ XHAmazingLoading/XHAmazingLoadingAnimationProtocol.h
@@ -0,0 +1,13 @@
+//
+//  XHAmazingLoadingAnimationProtocol.h
+//  XHAmazingLoadingExample
+//
+//  Created by Jack_iMac on 15/7/6.
+//  Copyright (c) 2015年 com.HUAJIE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol XHAmazingLoadingAnimationProtocol <NSObject>
+
+@end
