179f91dfb03d7eed51aeea788dac0dc601fda4af
Jack <xhzengAIB@gmail.com>
2015-07-08 16:48:01 +0800

    添加Skype的加载动画效果

---
+++ XHAmazingLoading/Animations/XHAmazingLoadingSkypeAnimation.h
@@ -0,0 +1,13 @@
+//
+//  XHAmazingLoadingSkypeAnimation.h
+//  XHAmazingLoadingExample
+//
+//  Created by Jack_iMac on 15/7/8.
+//  Copyright (c) 2015年 com.HUAJIE. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XHAmazingLoadingSkypeAnimation : NSObject
+
+@end
---
+++ XHAmazingLoading/Animations/XHAmazingLoadingSkypeAnimation.m
@@ -0,0 +1,13 @@
+//
+//  XHAmazingLoadingSkypeAnimation.m
+//  XHAmazingLoadingExample
+//
+//  Created by Jack_iMac on 15/7/8.
+//  Copyright (c) 2015年 com.HUAJIE. All rights reserved.
+//
+
+#import "XHAmazingLoadingSkypeAnimation.h"
+
+@implementation XHAmazingLoadingSkypeAnimation
+
+@end
